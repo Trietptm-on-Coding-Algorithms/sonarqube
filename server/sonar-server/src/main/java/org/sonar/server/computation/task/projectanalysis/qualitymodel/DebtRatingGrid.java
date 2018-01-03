262de758935087ef7192d8f0cefe61610167e335
Julien Lancelot <julien.lancelot@sonarsource.com>
2018-01-03 14:23:10 +0100

    SONAR-10193 Align Technical Debt rating calculation with docs

--- server/sonar-server/src/main/java/org/sonar/server/computation/task/projectanalysis/qualitymodel/DebtRatingGrid.java
+++ server/sonar-server/src/main/java/org/sonar/server/computation/task/projectanalysis/qualitymodel/DebtRatingGrid.java
@@ -20,20 +20,27 @@
 package org.sonar.server.computation.task.projectanalysis.qualitymodel;
 
 import com.google.common.annotations.VisibleForTesting;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 import org.sonar.api.config.Configuration;
-import org.sonar.api.utils.MessageException;
 
+import static com.google.common.base.Preconditions.checkState;
+import static java.lang.String.format;
 import static org.sonar.api.CoreProperties.RATING_GRID;
 import static org.sonar.api.CoreProperties.RATING_GRID_DEF_VALUES;
+import static org.sonar.server.computation.task.projectanalysis.qualitymodel.Rating.A;
+import static org.sonar.server.computation.task.projectanalysis.qualitymodel.Rating.B;
+import static org.sonar.server.computation.task.projectanalysis.qualitymodel.Rating.C;
+import static org.sonar.server.computation.task.projectanalysis.qualitymodel.Rating.D;
+import static org.sonar.server.computation.task.projectanalysis.qualitymodel.Rating.E;
 
 public class DebtRatingGrid {
 
   private final double[] gridValues;
-
-  public DebtRatingGrid(double[] gridValues) {
-    this.gridValues = Arrays.copyOf(gridValues, gridValues.length);
-  }
+  private final List<Function<Double, Rating>> gridFunctions;
 
   public DebtRatingGrid(Configuration config) {
     try {
@@ -42,20 +49,36 @@ public class DebtRatingGrid {
       for (int i = 0; i < 4; i++) {
         gridValues[i] = Double.parseDouble(grades[i]);
       }
+      this.gridFunctions = buildGridFunctions(gridValues);
     } catch (Exception e) {
       throw new IllegalArgumentException("The rating grid is incorrect. Expected something similar to '"
         + RATING_GRID_DEF_VALUES + "' and got '" + config.get(RATING_GRID).orElse("") + "'", e);
     }
   }
 
-  public Rating getRatingForDensity(double density) {
-    for (Rating rating : Rating.values()) {
-      double lowerBound = getGradeLowerBound(rating);
-      if (density >= lowerBound) {
-        return rating;
-      }
-    }
-    throw MessageException.of("The rating density value should be between 0 and " + Double.MAX_VALUE + " and got " + density);
+  public DebtRatingGrid(double[] gridValues) {
+    this.gridValues = Arrays.copyOf(gridValues, gridValues.length);
+    this.gridFunctions = buildGridFunctions(gridValues);
+  }
+
+  private static List<Function<Double, Rating>> buildGridFunctions(double[] gridValues) {
+    List<Function<Double, Rating>> gridFunctions = new ArrayList<>();
+    checkState(gridValues.length == 4, "Rating grid should contains 4 values");
+    gridFunctions.add(value -> value == 0 ? A : null);
+    gridFunctions.add(value -> isBetween(value, 0, gridValues[0]) ? A : null);
+    gridFunctions.add(value -> isBetween(value, gridValues[0], gridValues[1]) ? B : null);
+    gridFunctions.add(value -> isBetween(value, gridValues[1], gridValues[2]) ? C : null);
+    gridFunctions.add(value -> isBetween(value, gridValues[2], gridValues[3]) ? D : null);
+    gridFunctions.add(value -> value > gridValues[3] ? E : null);
+    return gridFunctions;
+  }
+
+  public Rating getRatingForDensity(double value) {
+    return gridFunctions.stream()
+      .map(f -> f.apply(value))
+      .filter(Objects::nonNull)
+      .findFirst()
+      .orElseThrow(() -> new IllegalArgumentException(format("Invalid value '%s'", value)));
   }
 
   public double getGradeLowerBound(Rating rating) {
@@ -68,6 +91,10 @@ public class DebtRatingGrid {
   @VisibleForTesting
   double[] getGridValues() {
     return gridValues;
+  }
+
+  private static boolean isBetween(double value, double min, double max) {
+    return value > min && value <= max;
   }
 
 }
